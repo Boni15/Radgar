91c1255b721cf7485fcba913d25a38e8bfbecec2
Boni15 <bonifaciodavid1999n@gmail.com>
2021-10-13 22:12:42 -0300

    Haciendo el nav

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+
+const NavBar = () => {
+    return (<div>
+             // Desarrolla tu implementación de un navbar dentro del componente NavBar.js
+             <NavBar />
+             <h2>Las ofertas de la semana</h2>
+     </div>)
+}
+ export default NavBar
