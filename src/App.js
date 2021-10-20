efad2922b54fd14f81df3d149b1f0390d41b38e6
Boni15 <bonifaciodavid1999n@gmail.com>
2021-10-20 20:24:02 -0300

    Tarea 4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import FeedbackMessage from './FeedBackMessage';
+// import { Col, Container, Row } from 'react-bootstrap';
 import './App.css';
-import NavBar from './components/NavBar';
+// va sin llaves porq cuando lo exportas como default no lleva
+import  {Navbar}  from './component/NavBar';
+import { Titulo } from './Titulo';
+// import { Image} from "react-bootstrap"
+import {AlertDismissible} from "./component/container/ItemListContainer"
+// import {Imagen} from "./component/CartWidget"
 
-function App() {
-  const objStyle= {backgroundColor: 'red'}
-  return (
-    
-    
-    <div className="App"  style={ objStyle } onClick={ () => alert('Hola') } >
-        <NavBar/>
-        <img src={logo} className="App-logo" alt="logo"  />
-        
-    </div>
-  );
+//
+
+// const Titulo = ({titulo})=>{
+//   return(
+//     <>
+//     <h2>{titulo}</h2>
+//     </>
+//   )
+// }
+
+ const App = () => {
+ 
+    return(
+     
+      <Titulo titulo="">
+        <AlertDismissible title ="Bienvenido al carrito de compras"/>
+          <h2> </h2>
+        <Navbar/>
+         
+       
+          
+       </Titulo> 
+      
+    )
+
 }
 
 export default App;
