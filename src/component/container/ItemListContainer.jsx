c333c8a7760cfb55fbaf5b9ec0bae084a25cf374
Boni15 <bonifaciodavid1999n@gmail.com>
2021-10-25 23:58:24 -0300

    Tarea 5

--- src/component/container/ItemListContainer.jsx
+++ src/component/container/ItemListContainer.jsx
@@ -1,26 +1,37 @@
+import React from "react";
+import ItemCount from "./ItemCount";
+import { Card } from "react-bootstrap";
 import {Alert, Button, } from "react-bootstrap"
 import { useState } from "react";
-       export const ItemListContainer =(props)=> {
-            const [show, setShow] = useState(true);
-          
+import { CardBody, CardText, CardTitle } from "reactstrap";
+
+       export const ItemListContainer =()=> {       
             return (
-              <>
-                <Alert show={show} variant="success">
-                  <Alert.Heading>{props.title}</Alert.Heading>
-                  <p>
+              <Card style={{width: "22rem"}}>
+              <CardBody>
+                  <CardTitle> card Title </CardTitle>
+                  <CardText> Some sadasdasdasdasdasdas</CardText>
+                <ItemCount stocl = {8} initial={1}/>
+              </CardBody>
+
+
+              </Card>
+              // <>
+              //   <Alert show={show} variant="success">
+              //     <Alert.Heading>{title}</Alert.Heading>
+              //     <p>
                     
-                  </p>
-                  <hr />
-                  <div className="d-flex justify-content-end">
-                    <Button onClick={() => setShow(false)} variant="outline-success">
-                      Comprar
-                    </Button>
-                  </div>
-                </Alert>
+              //     </p>
+              //     <hr />
+              //     <div className="d-flex justify-content-end">
+              //       <Button onClick={() => setShow(false)} variant="outline-success">
+              //         Comprar
+              //       </Button>
+              //     </div>
+              //   </Alert>
           
-                {!show && <Button onClick={() => setShow(true)}>Tu carrito</Button>}
-              </>
+              //   {!show && 
+              //   <Button onClick={() => setShow(true)}>carrito</Button>}
+              // </>
             );
           }
-          
-          
