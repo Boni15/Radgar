be2da12bb70410dc01f627ea27ddbf9828c6017b
Boni15 <bonifaciodavid1999n@gmail.com>
2021-11-08 18:22:34 -0300

    Tarea 7

--- src/component/CartWidget.jsx
+++ src/component/CartWidget.jsx
@@ -1,5 +1,5 @@
 import { Container,Row,Col,Image } from "react-bootstrap"
-
+import React from "react"
 const CartWidget =()=>{
     return (
         <Container>
@@ -15,4 +15,3 @@ const CartWidget =()=>{
     )
 }
 export default CartWidget
-
